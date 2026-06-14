d4817ec20c40c5693b07c6e9d2d2f9c8e2b7ba64
jirawatsadaon <jirawatsadaon55@gmail.com>
2026-06-14 16:11:51 +0700

    feat: add model training code (model_training folder)

--- detection_app/lib/core/constants/app_constants.dart
+++ detection_app/lib/core/constants/app_constants.dart
@@ -1,5 +1,3 @@
 class AppConstants {
-  // สำหรับ Android Emulator ใช้ 10.0.2.2
-  // สำหรับ iOS Simulator ใช้ 127.0.0.1
-  static const String baseUrl = "http://192.168.1.146:8000"; 
+  static const String baseUrl = "http://10.0.2.2:8000"; // IP Emulator
 }
